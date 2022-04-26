dc88ea78e4950215a5a3a9c13eac06fbff3567eb
hank770721 <hank770721@yahoo.com.tw>
2022-04-26 18:24:02 +0800

    調整資料庫密碼不須加密後判斷
    因參考OWASP安全, 故將資料庫密碼改存加密不存明碼

--- web/src/main/java/com/hkma/home/user/service/UserAccountService.java
+++ web/src/main/java/com/hkma/home/user/service/UserAccountService.java
@@ -2,10 +2,23 @@ package com.hkma.home.user.service;
 
 import java.util.Optional;
 
-import com.hkma.home.user.entity.UserAccountEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface UserAccountService {
-	public Optional<UserAccountEntity> findByUserId(String userId);
+import com.hkma.home.user.entity.UserAccountEntity;
+import com.hkma.home.user.repository.UserAccountRepository;
+
+@Service
+public class UserAccountService {
+	@Autowired
+	private UserAccountRepository userAccountRepository;
 	
-	public String getPasswordByUserId(String userId);
+	public Optional<UserAccountEntity> findByUserId(String userId) {
+		return userAccountRepository.findByUserId(userId);
+	}
+	
+	public String getPasswordByUserId(String userId) {
+        //return new BCryptPasswordEncoder().encode(userAccountRepository.getPasswordByUserId(userId));
+		return userAccountRepository.getPasswordByUserId(userId);
+    }
 }
